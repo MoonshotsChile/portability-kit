24d1210ccabf37a94911ec43eab49d9180ce943c
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-02-19 13:56:50 -0300

    api prefix

--- portability_kit.py
+++ portability_kit.py
@@ -5,7 +5,7 @@ from scrapper.banco_chile import BancoChile
 app = Flask(__name__)
 
 
-@app.route('/banco-chile/recipients', methods=['POST'])
+@app.route('/api/banco-chile/recipients', methods=['POST'])
 def banco_chile_recipients():
     username = request.json["username"]
     password = request.json["password"]
@@ -14,7 +14,7 @@ def banco_chile_recipients():
     return jsonify(entity.recipients())
 
 
-@app.route('/banco-chile/transactions', methods=['POST'])
+@app.route('/api/banco-chile/transactions', methods=['POST'])
 def banco_chile_transactions():
     username = request.json["username"]
     password = request.json["password"]
@@ -23,7 +23,7 @@ def banco_chile_transactions():
     return jsonify(entity.transactions())
 
 
-@app.route('/banco-chile/products', methods=['POST'])
+@app.route('/api/banco-chile/products', methods=['POST'])
 def banco_chile_products():
     username = request.json["username"]
     password = request.json["password"]
@@ -32,7 +32,7 @@ def banco_chile_products():
     return jsonify(entity.products())
 
 
-@app.route('/banco-chile/cards', methods=['POST'])
+@app.route('/api/banco-chile/cards', methods=['POST'])
 def banco_chile_cards():
     username = request.json["username"]
     password = request.json["password"]
@@ -41,7 +41,7 @@ def banco_chile_cards():
     return jsonify(entity.products())
 
 
-@app.route('/banco-chile/profile', methods=['POST'])
+@app.route('/api/banco-chile/profile', methods=['POST'])
 def banco_chile_profile():
     username = request.json["username"]
     password = request.json["password"]
